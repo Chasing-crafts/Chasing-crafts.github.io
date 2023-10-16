e651ada52a304ee8490181652109e6177a295976
chasecraft88 <112102369+chasecraft88@users.noreply.github.com>
2023-10-16 12:29:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
-// scripts.js
-// Function to handle smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
-  });
-});
-
-// Quiz related JavaScript
-// ... your quiz code here
-
 document.addEventListener("DOMContentLoaded", function () {
+  // Function to handle smooth scrolling
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+
+  // CyberMage section JavaScript
   const cyberMageSection = document.getElementById("cybermage-scroll-container");
   const headerText = document.getElementById("cybermage-header");
   const backgroundImages = ["cyber-bg-1.jpg", "cyber-bg-2.jpg", "cyber-bg-3.jpg"];
   let currentBackgroundIndex = 0;
 
   // Function to change the background image
-  function changeBackground() {
+  function changeBackgroundCyberMage() {
     currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
     const imageUrl = `url('images/${backgroundImages[currentBackgroundIndex]}')`;
     cyberMageSection.style.backgroundImage = imageUrl;
   }
 
   // Function to update header text
-  function updateHeaderText() {
+  function updateHeaderTextCyberMage() {
     const headerTexts = ["Explore the CyberMage World", "Unleash Your Powers", "Conquer the Digital Realm"];
     headerText.textContent = headerTexts[currentBackgroundIndex];
   }
 
   // Function to handle scrolling animations
-  function handleScrollAnimation() {
+  function handleScrollAnimationCyberMage() {
     if (window.scrollY > 200) {
       // Add animation when scrolled 200 pixels
       cyberMageSection.classList.add("animate");
@@ -46,37 +43,36 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // Add click event to change background
+  // Add click event to change background for CyberMage
   cyberMageSection.addEventListener("click", function () {
-    changeBackground();
-    updateHeaderText();
+    changeBackgroundCyberMage();
+    updateHeaderTextCyberMage();
   });
 
-  // Add scroll event to handle animations
-  window.addEventListener("scroll", handleScrollAnimation);
-});
+  // Add scroll event to handle animations for CyberMage
+  window.addEventListener("scroll", handleScrollAnimationCyberMage);
 
-document.addEventListener("DOMContentLoaded", function () {
+  // ChaseCraft section JavaScript
   const chaseCraftSection = document.getElementById("chasecraft-scroll-container");
-  const headerText = document.getElementById("chasecraft-header");
-  const backgroundImages = ["chasecraft-bg-1.jpg", "chasecraft-bg-2.jpg", "chasecraft-bg-3.jpg"];
-  let currentBackgroundIndex = 0;
+  const chaseCraftHeaderText = document.getElementById("chasecraft-header");
+  const chaseCraftBackgroundImages = ["chasecraft-bg-1.jpg", "chasecraft-bg-2.jpg", "chasecraft-bg-3.jpg"];
+  let chaseCraftCurrentBackgroundIndex = 0;
 
-  // Function to change the background image
-  function changeBackground() {
-    currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
-    const imageUrl = `url('images/${backgroundImages[currentBackgroundIndex]}')`;
+  // Function to change the background image for ChaseCraft
+  function changeBackgroundChaseCraft() {
+    chaseCraftCurrentBackgroundIndex = (chaseCraftCurrentBackgroundIndex + 1) % chaseCraftBackgroundImages.length;
+    const imageUrl = `url('images/${chaseCraftBackgroundImages[chaseCraftCurrentBackgroundIndex]}')`;
     chaseCraftSection.style.backgroundImage = imageUrl;
   }
 
-  // Function to update header text
-  function updateHeaderText() {
+  // Function to update header text for ChaseCraft
+  function updateHeaderTextChaseCraft() {
     const headerTexts = ["Embark on ChaseCraft Adventures", "Conquer New Worlds", "Join the ChaseCraft Community"];
-    headerText.textContent = headerTexts[currentBackgroundIndex];
+    chaseCraftHeaderText.textContent = headerTexts[chaseCraftCurrentBackgroundIndex];
   }
 
-  // Function to handle scrolling animations
-  function handleScrollAnimation() {
+  // Function to handle scrolling animations for ChaseCraft
+  function handleScrollAnimationChaseCraft() {
     if (window.scrollY > 200) {
       // Add animation when scrolled 200 pixels
       chaseCraftSection.classList.add("animate");
@@ -86,17 +82,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // Add click event to change background
+  // Add click event to change background for ChaseCraft
   chaseCraftSection.addEventListener("click", function () {
-    changeBackground();
-    updateHeaderText();
+    changeBackgroundChaseCraft();
+    updateHeaderTextChaseCraft();
   });
 
-  // Add scroll event to handle animations
-  window.addEventListener("scroll", handleScrollAnimation);
-});
+  // Add scroll event to handle animations for ChaseCraft
+  window.addEventListener("scroll", handleScrollAnimationChaseCraft);
 
-document.addEventListener("DOMContentLoaded", function () {
+  // Matrix Rain section JavaScript
   const matrixSection = document.getElementById("matrix-scroll-container");
   const matrixCanvas = document.createElement("canvas");
   matrixCanvas.id = "matrix-canvas";
@@ -158,177 +153,57 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
-// Function to handle smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
-  });
+
+// Quiz-related JavaScript
+const startQuizButton = document.getElementById("start-quiz-button");
+const quizContainer = document.getElementById("quiz-container");
+const submitAnswerButton = document.getElementById("submit-answer-button");
+const resultText = document.getElementById("result");
+
+let currentQuestion = 0;
+const questions = [
+  {
+    question: "What is the objective of Space Chase?",
+    correctAnswer: "b"
+  },
+  {
+    question: "In Stellar Odyssey, what can you explore?",
+    correctAnswer: "c"
+  },
+  {
+    question: "What is the main theme of Nebula Quest?",
+    correctAnswer: "a"
+  }
+};
+
+startQuizButton.addEventListener("click", function () {
+  startQuizButton.style.display = "none";
+  quizContainer.style.display = "block";
+  showQuestion(currentQuestion);
 });
 
-// ... The rest of the JavaScript code for CyberMage, ChaseCraft, and Matrix Rain
-// Function to handle smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
-  });
-});
+submitAnswerButton.addEventListener("click", function () {
+  const selectedAnswer = document.querySelector('input[name="answer"]:checked');
 
-// CyberMage section JavaScript
-document.addEventListener("DOMContentLoaded", function () {
-  const cyberMageSection = document.getElementById("cybermage-scroll-container");
-  const headerText = document.getElementById("cybermage-header");
-  const backgroundImages = ["cyber-bg-1.jpg", "cyber-bg-2.jpg", "cyber-bg-3.jpg"];
-  let currentBackgroundIndex = 0;
-
-  // Function to change the background image
-  function changeBackground() {
-    currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
-    const imageUrl = `url('images/${backgroundImages[currentBackgroundIndex]}')`;
-    cyberMageSection.style.backgroundImage = imageUrl;
-  }
-
-  // Function to update header text
-  function updateHeaderText() {
-    const headerTexts = ["Explore the CyberMage World", "Unleash Your Powers", "Conquer the Digital Realm"];
-    headerText.textContent = headerTexts[currentBackgroundIndex];
-  }
-
-  // Function to handle scrolling animations
-  function handleScrollAnimation() {
-    if (window.scrollY > 200) {
-      // Add animation when scrolled 200 pixels
-      cyberMageSection.classList.add("animate");
+  if (selectedAnswer) {
+    if (selectedAnswer.value === questions[currentQuestion].correctAnswer) {
+      resultText.textContent = "Correct!";
     } else {
-      // Remove animation if scrolled back up
-      cyberMageSection.classList remove("animate");
+      resultText.textContent = "Wrong. Try again!";
     }
-  }
 
-  // Add click event to change background
-  cyberMageSection.addEventListener("click", function () {
-    changeBackground();
-    updateHeaderText();
-  });
+    currentQuestion++;
 
-  // Add scroll event to handle animations
-  window.addEventListener("scroll", handleScrollAnimation);
-});
-
-// ChaseCraft section JavaScript
-document.addEventListener("DOMContentLoaded", function () {
-  const chaseCraftSection = document.getElementById("chasecraft-scroll-container");
-  const headerText = document.getElementById("chasecraft-header");
-  const backgroundImages = ["chasecraft-bg-1.jpg", "chasecraft-bg-2.jpg", "chasecraft-bg-3.jpg"];
-  let currentBackgroundIndex = 0;
-
-  // Function to change the background image
-  function changeBackground() {
-    currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
-    const imageUrl = `url('images/${backgroundImages[currentBackgroundIndex]}')`;
-    chaseCraftSection.style.backgroundImage = imageUrl;
-  }
-
-  // Function to update header text
-  function updateHeaderText() {
-    const headerTexts = ["Embark on ChaseCraft Adventures", "Conquer New Worlds", "Join the ChaseCraft Community"];
-    headerText.textContent = headerTexts[currentBackgroundIndex];
-  }
-
-  // Function to handle scrolling animations
-  function handleScrollAnimation() {
-    if (window.scrollY > 200) {
-      // Add animation when scrolled 200 pixels
-      chaseCraftSection.classList.add("animate");
+    if (currentQuestion < questions.length) {
+      showQuestion(currentQuestion);
     } else {
-      // Remove animation if scrolled back up
-      chaseCraftSection.classList.remove("animate");
+      resultText.textContent = "Quiz completed!";
     }
   }
-
-  // Add click event to change background
-  chaseCraftSection.addEventListener("click", function () {
-    changeBackground();
-    updateHeaderText();
-  });
-
-  // Add scroll event to handle animations
-  window.addEventListener("scroll", handleScrollAnimation);
 });
 
-// Matrix Rain section JavaScript
-document.addEventListener("DOMContentLoaded", function () {
-  const matrixSection = document.getElementById("matrix-scroll-container");
-  const matrixCanvas = document.createElement("canvas");
-  matrixCanvas.id = "matrix-canvas";
-  matrixSection.appendChild(matrixCanvas);
-
-  // Set up the canvas
-  const canvas = document.getElementById("matrix-canvas");
-  const context = canvas.getContext("2d");
-
-  // Matrix settings
-  const matrixFont = "Matrix Code NFI"; // You may need to import this font
-  const fontSize = 14;
-  const columns = Math.floor(window.innerWidth / fontSize);
-  const matrix = "01"; // Characters for the matrix rain
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  const drops = [];
-  for (let x = 0; x < columns; x++) {
-    drops[x] = Math.floor(Math.random() * canvas.height);
-  }
-
-  // Function to draw the matrix rain
-  function drawMatrix() {
-    context.fillStyle = "rgba(0, 0, 0, 0.04)";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    context.fillStyle = "#00FF00"; // Green color for matrix rain
-    context.font = `${fontSize}px ${matrixFont}`;
-
-    for (let i = 0; i < drops.length; i++) {
-      const text = matrix[Math.floor(Math random() * matrix.length)];
-      context.fillText(text, i * fontSize, drops[i] * fontSize);
-
-      if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
-        drops[i] = 0;
-      }
-
-      drops[i]++;
-    }
-  }
-
-  // Function to update the matrix rain animation
-  function updateMatrixRain() {
-    drawMatrix();
-    requestAnimationFrame(updateMatrixRain);
-  }
-
-  updateMatrixRain();
-
-  // Add click event to pause/resume the animation
-  matrixSection.addEventListener("click", function () {
-    if (canvas.style.display === "none") {
-      canvas.style.display = "block";
-      updateMatrixRain();
-    } else {
-      canvas.style.display = "none";
-    }
-  });
-});
+function showQuestion(questionIndex) {
+  resultText.textContent = "";
+  document.getElementById("question").textContent = questions[questionIndex].question;
+  document.querySelectorAll('input[name="answer"]').forEach((radio) => (radio.checked = false));
+}
